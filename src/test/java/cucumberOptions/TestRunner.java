979a9963e606eb42570e24ab64f95afe2de45a2a
H <H@DESKTOP-5BVDV86.mshome.net>
2022-07-08 21:07:08 +0700

    Run scenario with parameter and non_para

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -10,10 +10,11 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features = "src/test/java/features",
 		glue = "stepsDefinitions",
+		//dryRun = true,
 		monochrome = true,
 		plugin = {"pretty","html:target/site/cucumber-report-default","json:target/site/cucumber.json"},
 		snippets = SnippetType.CAMELCASE,
-		tags = {"@displayed"})
+		tags = {" @datatable_step"})
 
 public class TestRunner {
 
--- src/test/java/stepsDefinitions/FacebookSteps.java
+++ src/test/java/stepsDefinitions/FacebookSteps.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FacebookSteps {
@@ -20,19 +22,50 @@ public class FacebookSteps {
 		WebDriverManager.firefoxdriver().setup();
 		driver = new FirefoxDriver();
 		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-	@Then("^Verify email textbox is displayed$")
-	public void verifyEmailTextboxIsDisplayed()  {
-		Assert.assertTrue(driver.findElement(By.xpath("//input[@id='email']")).isDisplayed());
-
+	@When("^Input to Username textbox$") 
+	public void inputToUsernameTextbox() {
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys("automation@gmail.com");
+	}
+	@When("^Input to Password textbox$") 
+	public void inputToPasswordTextbox() {
+		driver.findElement(By.id("pass")).clear();
+		driver.findElement(By.id("pass")).sendKeys("automation");
+	}
+	
+	@When("^Input to Username textbox with \"([^\"]*)\"$") 
+	public void inputToUsernameTextboxWith(String email) {
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys(email);
+	}
+	@When("^Input to Password textbox with \"([^\"]*)\"$") 
+	public void inputToPasswordTextboxWith(String password) {
+		driver.findElement(By.id("pass")).clear();
+		driver.findElement(By.id("pass")).sendKeys(password);
+	}
+	
+	@When("^Input to Username with \"([^\"]*)\" and Password with \"([^\"]*)\"$")
+	public void InpuToUsernameWithAndPasswordWith(String email, String password){
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys(email);
+		driver.findElement(By.id("pass")).clear();
+		driver.findElement(By.id("pass")).sendKeys(password);
+	}
+	
+	 @When("^Input to Username and Password$")
+	    public void inputToUsernameAndPassword(DataTable table) {
+	        
+	    }
+	
+	@When("^Click to Submit button$") 
+	public void clickToSubmitButton() {
+		driver.findElement(By.name("login")).click();
 	}
 
-	@Then("^verify password textbox is displayed$")
-	public void verifyPasswordTextboxIsDisplayed()  {
-		Assert.assertTrue(driver.findElement(By.xpath("//input[@id='pass']")).isDisplayed());
-	}
 	
 	@And("^Close application$")
 	public void closeApplication()  {
